cd5296be998a323aea4a2cdcc799aaf77f85514b
Lex Miin <alexmiyin@gmail.com>
2024-04-10 14:07:48 +0300

    feat: enable CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import SQLModel
 
 from app.api.main import api_router
@@ -7,5 +8,15 @@ from app.db.main import engine
 SQLModel.metadata.create_all(engine)
 
 app = FastAPI()
+
+origins = ['http://localhost:5173']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allow_headers=['Accept', 'Authorization', 'Content-Type', 'X-CSRF-Token'],
+)
 
 app.include_router(api_router)
