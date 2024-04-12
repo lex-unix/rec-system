006d0929ae6f6dcccca233b6144aca5e6b2b2e33
Lex Miin <alexmiyin@gmail.com>
2024-04-12 14:48:01 +0300

    chore: update run script to start containers

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env fish
 
-load_env .env; or exit
+if test -e $VIRTUAL_ENV
+  source $(path resolve "./venv/bin/activate.fish")
+end
 
+set -l services db redis
+set -l running_containers (docker compose ps --format "{{.Names}}")
+set -l not_running false
+
+for service in $services
+  if not string match -q "*-$service-*" $running_containers
+    echo "Starting service: $service"
+    docker compose up -d $service
+    set not_running true
+  end
+end
+
+if not $not_running
+  echo "All containers are already running."
+end
+
+load_env .env; or exit
 echo ".env file loaded"
 
 uvicorn app.main:app --reload
