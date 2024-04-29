873c6cda839917e7bb38552b994ef842d0df5a35
Lex Miin <alexmiyin@gmail.com>
2024-04-29 13:32:44 +0300

    fix: don't add each foreign key twice

--- migrations/000005_create_chat_messages_table.up.sql
+++ migrations/000005_create_chat_messages_table.up.sql
@@ -2,10 +2,7 @@ CREATE TABLE IF NOT EXISTS chat_messages (
   id bigserial PRIMARY KEY,
   content text NOT NULL,
   created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-  chat_id bigserial REFERENCES chats(id) NOT NULL,
-  sender_id bigserial REFERENCES users(id) NOT NULL,
-
-  FOREIGN KEY(chat_id) REFERENCES chats(id),
-  FOREIGN KEY(sender_id) REFERENCES users(id)
+  chat_id bigint REFERENCES chats(id) NOT NULL,
+  sender_id bigint REFERENCES users(id) NOT NULL
 );
 
